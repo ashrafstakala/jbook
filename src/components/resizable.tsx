ffaa9c1d6f44ece37247ee50bb5a44105d9ca3ee
ashrafstakala <ashrafstakala@gmail.com>
2022-05-26 00:27:23 +0200

    code editor resizing

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,14 +1,14 @@
 import './resizable.css';
 const { ResizableBox } = require('react-resizable');
 
-interface ResizableProps2 {
+interface ResizableProps {
   direction: 'horizontal' | 'vertical';
   children: React.ReactNode;
 }
 
-const Resizable: React.FC<ResizableProps2> = ({ direction, children }) => {
+const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   return (
-    <ResizableBox width={300} height={300} resizeHandles={['s']}>
+    <ResizableBox width={Infinity} height={300} resizeHandles={['s']}>
       {children}
     </ResizableBox>
   );
