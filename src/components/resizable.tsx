1474893bc27242f79c03f0766a5e4a3db3d2ab26
ashrafstakala <ashrafstakala@gmail.com>
2022-05-26 19:42:25 +0200

    horizontal preview resizing

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,5 +1,5 @@
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 import './resizable.css';
-const { ResizableBox } = require('react-resizable');
 
 interface ResizableProps {
   direction: 'horizontal' | 'vertical';
@@ -7,17 +7,28 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return (
-    <ResizableBox
-      maxConstraints={[Infinity, window.innerHeight * 0.9]}
-      minConstraints={[Infinity, 24]}
-      width={Infinity}
-      height={300}
-      resizeHandles={['s']}
-    >
-      {children}
-    </ResizableBox>
-  );
+  let resizableProps: ResizableBoxProps;
+
+  if (direction === 'horizontal') {
+    resizableProps = {
+      className: 'resize-horizontal',
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+      width: window.innerWidth * 0.75,
+      height: Infinity,
+      resizeHandles: ['e'],
+    };
+  } else {
+    resizableProps = {
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      minConstraints: [Infinity, 24],
+      width: Infinity,
+      height: 300,
+      resizeHandles: ['s'],
+    };
+  }
+
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
