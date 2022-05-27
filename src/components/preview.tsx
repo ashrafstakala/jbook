c30cf968256472009a58cac98fd2cc4fec071c57
ashrafstakala <ashrafstakala@gmail.com>
2022-05-27 18:41:59 +0200

    print bundling error in preview

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -46,8 +46,6 @@ const Preview: React.FC<PreviewProps> = ({ code, err }) => {
     }, 50);
   }, [code]);
 
-  console.log(err);
-
   return (
     <div className="preview-wrapper">
       <iframe
@@ -56,6 +54,7 @@ const Preview: React.FC<PreviewProps> = ({ code, err }) => {
         sandbox="allow-scripts"
         srcDoc={html}
       />
+      {err && <div className="preview-error">{err}</div>}
     </div>
   );
 };
