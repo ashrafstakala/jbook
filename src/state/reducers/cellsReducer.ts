84d21f98642e3b436c8de09c69195ab5dddcafc9
ashrafstakala <ashrafstakala@gmail.com>
2022-05-28 14:17:12 +0200

    move cell case type

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -32,10 +32,23 @@ const reducer = produce((state = initialState, action: Action) => {
       return;
 
     case ActionType.MOVE_CELL:
-      return state;
+      const { direction } = action.payload;
+      const index = state.order.findIndex(
+        (id: any) => id === action.payload.id
+      );
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return;
+      }
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+      return;
 
     case ActionType.INSERT_CELL_BEFORE:
       return state;
+
     default:
       return state;
   }
