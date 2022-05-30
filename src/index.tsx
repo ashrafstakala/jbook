a3ff39d4b032c311afb2106c12639597926edcfb
ashrafstakala <ashrafstakala@gmail.com>
2022-05-30 21:42:07 +0200

    adding button icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { Provider } from 'react-redux';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import ReactDOM from 'react-dom';
 import { store } from './state';
 import CellList from './components/cell-list';
